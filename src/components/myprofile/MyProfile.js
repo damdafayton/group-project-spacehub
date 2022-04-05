484d812aa8003603c334093e0f4fb9b6c50edd44
backtofayton <yucelolcay@gmail.com>
2022-04-05 23:13:37 +0300

    Add myprofile rockets list

--- src/components/myprofile/MyProfile.js
+++ src/components/myprofile/MyProfile.js
@@ -1,13 +1,18 @@
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function MyProfile() {
-  const rockets = useSelector((state) => state.rockets)
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
 
   return (
     <section className="mx-5">
       <h2>My Rockets</h2>
       <ul className="list-group">
+        {reservedRockets.map((rocket) => (
+          <li key={uuidv4()} className="list-group-item">{rocket.name}</li>
+        ))}
       </ul>
     </section>
-  )
+  );
 }
