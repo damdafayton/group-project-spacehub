9bc709caabd137b98c4f4d79dce182e45e857e59
Dagi <dagi.zewdu.dz@gmail.com>
2022-04-06 14:13:22 +0300

    Implemented git revert

--- src/redux/configure.js
+++ src/redux/configure.js
@@ -3,24 +3,11 @@ import { applyMiddleware, createStore, combineReducers } from 'redux';
 // Logger with default options
 import logger from 'redux-logger';
 
-<<<<<<< HEAD
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['mission', 'rocket'],
-};
-=======
 import rocketsReducer from './rockets/rocketsReducer';
->>>>>>> parent of 40cc77a... Added mission with redux persist
 
 const rootReducer = combineReducers(
   {
-<<<<<<< HEAD
-    rocket: rocketsReducer,
-    mission: MissionReducer,
-=======
     rockets: rocketsReducer,
->>>>>>> parent of 40cc77a... Added mission with redux persist
   },
 );
 
