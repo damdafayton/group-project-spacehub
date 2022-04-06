0d74a36e64d04274ce16bae16fa9b21a3b6dd7a8
Dagi <dagi.zewdu.dz@gmail.com>
2022-04-06 14:14:43 +0300

    git revert applied

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import './css/bootstrap.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './redux/configure';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import store from './redux/configure';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
