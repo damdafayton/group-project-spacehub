8522f3389686a198b85034d48931edc865d188d3
Dagi <dagi.zewdu.dz@gmail.com>
2022-04-06 15:20:23 +0300

    Added data featching rendering missions and also join and leaving mission button

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import MissionsContainer from './components/missions/missionContainer';
 import MyProfile from './components/myprofile/MyProfile';
 import Rockets from './components/rockets/Rockets';
 
@@ -7,7 +8,7 @@ function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Rockets />} />
-      <Route path="/mission" element={<div className="text-center">UNDER CONSTRUCTION</div>} />
+      <Route path="/mission" element={<MissionsContainer />} />
       <Route path="/myprofile" element={<MyProfile />} />
     </Routes>
   );
