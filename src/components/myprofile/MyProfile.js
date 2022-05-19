69ed4abdffa30c56b9c82f192876fe0ebca74c20
backtofayton <yucelolcay@gmail.com>
2022-05-19 22:15:11 +0300

    Design refactor

--- src/components/myprofile/MyProfile.js
+++ src/components/myprofile/MyProfile.js
@@ -8,9 +8,9 @@ export default function MyProfile() {
   const selectMission = (id) => Missions.find((mission) => mission.mission_id === id);
   return (
     <section className="container">
-      <div className="row">
+      <div className="row gy-3">
         <div className="col-lg-6">
-          <h2>My Rockets</h2>
+          <h2 className="fs-4">My Rockets</h2>
           <ul className="list-group">
             {reservedRockets.length
               ? reservedRockets.map((rocket) => (
@@ -19,7 +19,7 @@ export default function MyProfile() {
           </ul>
         </div>
         <div className="col-lg-6">
-          <h2>Joined Missions</h2>
+          <h2 className="fs-4"`>Joined Missions</h2>
           <ul className="list-group">
             {joinedMission.length
               ? joinedMission.map((id) => {
